c4c41989ef7933ba619c8c11c5fa4eca415fe941
Meego Smith <meegosvk@gmail.com>
2022-10-14 16:13:14 +0100

    TinyDB implementation, first glance at structure

---
+++ memorydb.py
@@ -0,0 +1,37 @@
+from tinydb import TinyDB, where, Query
+from tinydb.storages import MemoryStorage
+import datetime
+import statistics
+from time import sleep
+db = TinyDB(storage=MemoryStorage)
+q = Query()
+
+class TemperatureStore:
+  """
+    TO DO:
+     - menitelny pocet udrziavanich hodnot pri inicializacii classy
+     - menitelny pocet hodnot z ktorych sa vyratava priemer
+       - Pravdepodobne to budeme nastavovat na classe. Aby sa to drzalo pocas celej instancie
+     - Vytvorime tabulku per sensor, nech sa to lahsie udrzuje
+       - k tomu treba prisposobit citanie, zapisovanie a mazanie
+  """
+
+  def mean():
+    """
+    Vrati priemernu hodnotu za N hodnot a odmaze to, co je navyse, aby sme udrziavali malo alokovanej pamate.
+    """
+    try:
+      temp_array = [x['temp'] for x in db.all()[-5:]]
+    except IndexError:
+      print('Not enough resources')
+    else:
+      print(temp_array)
+      avarage = statistics.mean(temp_array)
+      try:
+        db.remove(doc_ids=[x.doc_id for x in db.search(q.time < db.all()[-5]['time'])])
+      except IndexError:
+        print('Nothing to delete')
+      return avarage
+
+  def add_value(self, temperature, time = datetime.datetime.now()):
+    db.insert({'sensor_mac': self.sensor_mac, 'time': time, 'temp': temperature})
