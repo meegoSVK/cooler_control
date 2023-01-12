83ae53851f650b3d09ee4195c403067426ebe2d9
Meego Smith <meegosvk@gmail.com>
2023-01-12 20:06:01 +0000

    memorydb.py  - table for each sensor, created test.py file with basic test case - read each probe, store in TinyDB, get mean value and sleep for 5 seconds

--- memorydb.py
+++ memorydb.py
@@ -18,23 +18,24 @@ class TemperatureStore:
   
   def __init__(self, sensor_mac):
     self.sensor_mac = sensor_mac
+    self.table = db.table(sensor_mac)
 
   def mean(self):
     """
     Vrati priemernu hodnotu za N hodnot a odmaze to, co je navyse, aby sme udrziavali malo alokovanej pamate.
     """
     try:
-      temp_array = [x['temp'] for x in db.all()[-5:]]
+      temp_array = [x['temp'] for x in self.table.all()[-5:]]
     except IndexError:
       print('Not enough resources')
     else:
       print(temp_array)
       avarage = statistics.mean(temp_array)
       try:
-        db.remove(doc_ids=[x.doc_id for x in db.search(q.time < db.all()[-5]['time'])])
+        self.table.remove(doc_ids=[x.doc_id for x in self.table.search(q.time < self.table.all()[-5]['time'])])
       except IndexError:
         print('Nothing to delete')
       return avarage
 
   def add_value(self, temperature, time = datetime.datetime.now()):
-    db.insert({'sensor_mac': self.sensor_mac, 'time': time, 'temp': temperature})
+    self.table.insert({'sensor_mac': self.sensor_mac, 'time': time, 'temp': temperature})
---
+++ test.py
@@ -0,0 +1,19 @@
+import temp_check
+import memorydb
+import time
+import glob
+import os
+
+sensor_path = '/sys/bus/w1/devices/'
+rng = range(1,10)
+list_of_sensors = glob.glob(sensor_path+'28*')
+
+while True:
+  for s in list_of_sensors:
+    sns = temp_check.Sensor(os.path.basename(s))
+    temp_store = memorydb.TemperatureStore(s)
+    temperature, timestmp = sns.get_celsius()
+    temp_store.add_value(temperature, timestmp)
+    print(os.path.basename(s), ' ',temp_store.mean())
+  time.sleep(5)
+  print()
