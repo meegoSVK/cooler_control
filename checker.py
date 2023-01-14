9ac2fe10acd133d25aae4e5c9b9d6a37cd025727
Meego Smith <meegosvk@gmail.com>
2023-01-14 19:20:24 +0000

    Checker creation

---
+++ checker.py
@@ -0,0 +1,14 @@
+class Checker:
+  def __init__(self, var_type, value):
+    self.var_type = var_type
+    self.value = value
+  def do_str(self):
+    return str(self.value)
+  def do_int(self):
+    return int(self.value)
+  def do_float(self):
+    return float(self.value)
+  def do_check(self):
+    do = f'do_{self.var_type}'
+    if hasattr(self, do) and callable(func := getattr(self, do)):
+      func()
