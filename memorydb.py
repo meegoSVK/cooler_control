97674223838efb893d2e38295fb83d222c31c594
Meego Smith <meegosvk@gmail.com>
2023-01-16 19:34:02 +0000

    Small polishing. Moved global variables into class

--- memorydb.py
+++ memorydb.py
@@ -3,20 +3,26 @@ from tinydb.storages import MemoryStorage
 import datetime
 import statistics
 from time import sleep
-db = TinyDB(storage=MemoryStorage)
-q = Query()
+
 
 class TemperatureStore:
   """
+  Here we store and maintain incoming values for storing temperature data.
+  We can have instance for any number of sensors, becauces we create table in memory based on incoming sensor_id.
   """
-  
+  global db
+  global q
+  db = TinyDB(storage=MemoryStorage)
+  q = Query()
+
+
   def __init__(self, sensor_mac):
     self.sensor_mac = sensor_mac
     self.table = db.table(sensor_mac)
 
   def mean(self, retention):
     """
-    Vrati priemernu hodnotu za N hodnot a odmaze to, co je navyse, aby sme udrziavali malo alokovanej pamate.
+    Returns mean value in N values and deletes what is above definued number of values.
     """
     try:
       temp_array = [x['temp'] for x in self.table.all()[-retention:]]
@@ -32,4 +38,7 @@ class TemperatureStore:
       return avarage
 
   def add_value(self, temperature, time = datetime.datetime.now()):
+    """
+    Adding new value to memory table.
+    """
     self.table.insert({'sensor_mac': self.sensor_mac, 'time': time, 'temp': temperature})
